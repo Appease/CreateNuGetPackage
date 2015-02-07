79b96419f3b6b80a09eb738482052f51dd36efe2
Chris Dostert <chris.dostert@live.com>
2015-02-07 00:13:34 -0800

    added packages.config

--- Source/Posh-CI-CreateNuGetPackage.psm1
+++ Source/Posh-CI-CreateNuGetPackage.psm1
@@ -1,21 +1,10 @@
 # halt immediately on any errors which occur in this module
 $ErrorActionPreference = 'Stop'
 
-function EnsureNuGetCommandLineInstalled(){
-    # install nuget-commandline
-    try{
-        Get-Command nuget -ErrorAction 'Stop' | Out-Null
-    }
-    catch{             
-        cinst 'nuget.commandline'
-    }    
-}
-
 function Invoke-CIStep(
 [string[]][Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]$CsprojAndOrNuspecFilePaths,
 [string][Parameter(ValueFromPipelineByPropertyName = $true)]$OutputDirectoryPath='.',
 [string][Parameter(ValueFromPipelineByPropertyName = $true)]$Version='0.0.1'){
-    EnsureNuGetCommandLineInstalled
     
     foreach($csprojOrNuspecFilePath in $CsprojAndOrNuspecFilePaths)
     {        
